e39776e8e0f92e5caec5f26c3332a9ce28295efa
Artiom Khandamirov <t9max@yandex.ru>
2022-07-02 19:06:35 +0300

    Add sample Shell script

---
+++ run_all.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+. ./.env
+
+TMP_DIRECTORY="$(mktemp -d)"
+trap 'rm -rf -- "$TMP_DIRECTORY"' EXIT
+
+poetry run python ./news_fetcher/news_fetcher.py --source-module rss --data-file "$DATA_FILE" --source-path "$SOURCE_PATH" --source-name "$SOURCE_NAME" fetch-news
+poetry run python ./news_fetcher/news_fetcher.py --source-module rss --data-file "$DATA_FILE" --source-path "$SOURCE_PATH" --source-name "$SOURCE_NAME" fetch-news-pages
+mkdir "$TMP_DIRECTORY/pages"
+poetry run python ./news_fetcher/news_fetcher.py --source-module rss --data-file "$DATA_FILE" --source-path "$SOURCE_PATH" --source-name "$SOURCE_NAME" generate-wiki-pages --output-file "$TMP_DIRECTORY/pages.json" --output-directory "$TMP_DIRECTORY/pages"
+
+CURRENT_DIRECTORY="$(pwd)"
+
+cd "$WIKI_TOOL_DIRECTORY"
+
+poetry run python ./wiki_tool_python/wikitool.py upload-pages "$TARGET_API_URL" "$TMP_DIRECTORY/pages" "$TMP_DIRECTORY/pages.json" --prefix "$WIKI_PREFIX" --dictionary --extended-dictionary
+
+cd "$CURRENT_DIRECTORY"
+
+poetry run python ./news_fetcher/news_fetcher.py --source-module rss --data-file "$DATA_FILE" --source-path "$SOURCE_PATH" --source-name "$SOURCE_NAME" mark-uploaded-pages --input-file "$TMP_DIRECTORY/pages.json"
